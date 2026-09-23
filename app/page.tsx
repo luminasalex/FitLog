f58c393e5a63f407a692cce77451893016fae83d
alexhalder <alexhalder2007@gmail.com>
2026-09-23 21:46:25 +0600

    data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./component/hero";
+import Library from "./component/TheLibrary/library";
 
 const page = () => {
   return (
     <div>
       <Hero />
+      <Library />
     </div>
   );
 };
